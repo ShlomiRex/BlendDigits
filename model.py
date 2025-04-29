7362c9f7584428740cad96b0d7cd6c4564e544a7
Shlomi <vgtvgy1@gmail.com>
2025-04-29 04:20:11 +0300

    save

--- model.py
+++ model.py
@@ -58,6 +58,9 @@ class VariationalAutoencoder(nn.Module):
         Variance is exponential of log_var
         """
         epsilon = torch.randn_like(var).to(self.device)
+        mean = mean.to(self.device)
+        var = var.to(self.device)
+
         z = mean + var * epsilon
         return z
 
@@ -77,7 +80,7 @@ class VariationalAutoencoder(nn.Module):
         # Return x hat
         return x_reconstructed, mean, log_var
 
-class DefaultVAE(VariationalAutoencoder):
+class InterpolationModel(VariationalAutoencoder):
     def __init__(self):
         # Default parameters for the VAE model
         input_dim = 1 * 28 * 28
@@ -88,4 +91,73 @@ class DefaultVAE(VariationalAutoencoder):
         # Load the model
         vae = VariationalAutoencoder(input_dim, hidden_dim, latent_dim).to(device)
         vae.load_state_dict(torch.load("vae_model.pth", map_location=device))
-        super(DefaultVAE, self).__init__(input_dim=784, hidden_dim=400, latent_dim=20, device=device)
+        vae.eval() # Set the model to evaluation mode, we don't want to train it again
+        vae.to(device)
+        super(InterpolationModel, self).__init__(input_dim=input_dim, hidden_dim=hidden_dim, latent_dim=latent_dim, device=device)
+
+        self.vae = vae
+
+    
+    def forward(self, input_img1: torch.Tensor, input_img2: torch.Tensor, interpolation: float) -> torch.Tensor:
+        # Encode the two images
+        mean1, log_var1 = self.encode(input_img1)
+        mean2, log_var2 = self.encode(input_img2)
+
+        # Reparameterization trick for both images
+        z1 = self.reparameterization(mean1, torch.exp(0.5 * log_var1))
+        z2 = self.reparameterization(mean2, torch.exp(0.5 * log_var2))
+
+        # Interpolate between the two latent vectors
+        latent_vector = (1 - interpolation) * z1 + interpolation * z2 # Linear interpolation
+
+        # Decode interpolation to image
+        interpolated_image = self.vae.decode(latent_vector)
+
+        return interpolated_image.squeeze(0) # Remove the batch dimension only (keep channel dimension 1)
+
+if __name__ == "__main__":
+    from PIL import Image
+    import torchvision.transforms as transforms
+    import matplotlib.pyplot as plt
+
+    # Example usage
+    model = InterpolationModel()
+
+    # Read images from local file
+    transform = transforms.ToTensor()
+    
+    image = Image.open('frontend/public/mnist/1/1_1.png').convert('L')
+    input_img1 = transform(image).to('cpu')
+
+    image = Image.open('frontend/public/mnist/2/2_1.png').convert('L')
+    input_img2 = transform(image).to('cpu')
+
+    # Run the model - output is image
+    interpolation = 0.5  # Interpolation factor (0.0 to 1.0)
+    interpolated_image = model(input_img1.unsqueeze(0), input_img2.unsqueeze(0), interpolation)
+
+    # Remove batch and channel dimensions for plotting
+    img1 = input_img1.squeeze().numpy()
+    img2 = input_img2.squeeze().numpy()
+    interp = interpolated_image.squeeze().detach().cpu().numpy()
+
+    # Plot the images in a row: img1, interpolated, img2
+    plt.figure(figsize=(9, 3))
+
+    plt.subplot(1, 3, 1)
+    plt.imshow(img1, cmap='gray')
+    plt.axis('off')
+    plt.title('Image 1')
+
+    plt.subplot(1, 3, 2)
+    plt.imshow(interp, cmap='gray')
+    plt.axis('off')
+    plt.title('Interpolated')
+
+    plt.subplot(1, 3, 3)
+    plt.imshow(img2, cmap='gray')
+    plt.axis('off')
+    plt.title('Image 2')
+
+    plt.tight_layout()
+    plt.show()
